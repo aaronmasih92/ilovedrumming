1f14cbf038dac797c7f630c6c6a37629fea9848d
Aaron Masih <aaron@ashers-mbp.lan>
2018-07-07 18:51:14 -0400

    mayb this will fix the amazon problem

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -7,6 +7,6 @@ CarrierWave.configure do |config|
       region:                'us-east-2'                  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'aaronmasihtest'                                   # required
-    config.fog_public     = false                                                 # optional, defaults to true
+    config.fog_public     = true                                                 # optional, defaults to true
     config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' } # optional, defaults to {}
   end
